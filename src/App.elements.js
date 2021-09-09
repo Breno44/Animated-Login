4fe8e126d8134cff26b1f2942f692311a9f8205d
Breno Henrique <bhfs.2908@gmail.com>
2021-09-09 16:06:42 -0300

    feat:add styled components

---
+++ src/App.elements.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const Wave = styled.img`
+  position: fixed;
+  height: 100vh;
+  left: 0;
+  bottom: 0;
+  z-index: -1;
+`;
+
+export const ImgBg = styled.img`
+  width: 500px;
+`;
+
+export const ImgAvatar = styled.img`
+  width: 100px;
+`;
+
+export const MainContainer = styled.div`
+  font-family: "Poppins", sans-serif;
+`;
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 7rem;
+  padding: 0 2rem;
+`;
+
+export const Img = styled.div`
+  width: 500px;
+`;
+
+export const LoginContent = styled.div``;
+
+export const Form = styled.form``;
+
+export const Title = styled.h2``;
+
+export const InputOne = styled.div``;
+
+export const Icon = styled.div``;
+
+export const Icon1 = styled.i``;
+
+export const Icon2 = styled.i``;
+
+export const D = styled.div``;
+
+export const Input = styled.input``;
+
+export const InputPass = styled.div``;
+
+export const A = styled.a``;
+
+export const Button = styled.button``;
